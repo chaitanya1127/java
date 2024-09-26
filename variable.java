0fb32fb10124bc4e99e9e90cc349980dfb6ca954
chaitanya1127 <cchaitu288@gmail.com>
2024-09-26 15:22:43 +0530

    variables and datatypes

---
+++ variable.java
@@ -0,0 +1,46 @@
+public class variable {
+
+	public static void main(String[] args) {
+		byte numberOfEmployeesInRoom = 18;
+		byte departmentId = 27;
+		byte numberOfDepartmentsInCollege = 30;
+		byte vowelsInAnAlpthabets = 5;
+		byte colorsInTrafficSignal = 3;
+		byte priceOfBook = 40;
+		byte workingHoursPerDay = 9;
+		byte buildingsInColony = 30;
+		System.out.println("departmentId = "+departmentId);
+		System.out.println("priceOfBook = "+priceOfBook);
+		System.out.println("workingHoursPerDay ="+workingHoursPerDay);
+		System.out.println("buildingsInColony ="+buildingsInColony);
+		departmentId = 11;
+		priceOfBook = 66;
+	    workingHoursPerDay = 10;
+	    buildingsInColony = 40;
+	    System.out.println("departmentId = "+departmentId);
+		System.out.println("priceOfBook = "+priceOfBook);
+		System.out.println("workingHoursPerDay ="+workingHoursPerDay);
+		System.out.println("buildingsInColony ="+buildingsInColony);
+		short totalDaysInYear = 365;
+		short numberOfStudentsInClass = 120;
+		short numberOfSystemsWorkingInLab = 11;
+		short productCode = 6847 ;
+		short costOfBag = 700;
+		short workersInIndustry = 48;
+		short numberOfBooksInRack = 50;
+		short countOfSwitchesInBoard = 6;
+		System.out.println("numberOfSystemsWorkingInLab = "+numberOfSystemsWorkingInLab);
+		System.out.println("productCode = "+productCode);
+		System.out.println("numberOfBooksInRack ="+numberOfBooksInRack);
+		System.out.println("countOfSwitchesInBoard ="+countOfSwitchesInBoard);
+		numberOfSystemsWorkingInLab = 27;
+		productCode = 6846;
+		numberOfBooksInRack = 55;
+		countOfSwitchesInBoard = 8;
+		System.out.println("numberOfSystemsWorkingInLab = "+numberOfSystemsWorkingInLab);
+		System.out.println("productCode = "+productCode);
+		System.out.println("numberOfBooksInRack ="+numberOfBooksInRack);
+		System.out.println("countOfSwitchesInBoard ="+countOfSwitchesInBoard);
+	}
+
+}
