bceaa4c4bbae17832ff2e23bf8caab0d0104be6d
chaitanya1127 <cchaitu288@gmail.com>
2024-10-10 16:59:54 +0530

    state and behaviour

---
+++ Train.java
@@ -0,0 +1,24 @@
+public class Train {
+	public static void main(String[] args) {
+	
+	}
+ 
+       String trainType;
+       int trainNumber;
+       String destination;
+       int capacity;
+       int currentPassengers;
+
+
+
+     public static void start() {
+     }
+     public static void stop() {
+     }
+     public static void changeDestination () {
+     }
+     public static void announcement () {
+      
+     }
+}
+
