98b89efdab18b6145bb9454e4b871b2f43c8176f
chaitanya1127 <cchaitu288@gmail.com>
2024-10-10 16:47:44 +0530

    objects and class

---
+++ Train_details.java
@@ -0,0 +1,145 @@
+  public class Train_details {
+	int trainNumber;
+	String destination;
+	int capacity;
+	String type;
+	int currentPassengers;
+
+	public static void main(String[] args) {
+		Train train = new Train();
+		train.trainNumber = 1233;
+		train.destination = " bengaluru";
+		train.capacity = 2000;
+		train.trainType = " express";
+		train.currentPassengers = 1200;
+		
+                System.out.println("trainNumber :"+train.trainNumber);
+                System.out.println("destination :"+train.destination);
+                System.out.println("capacity :"+train.capacity);
+                System.out.println("trainType :"+train.trainType);
+                System.out.println("currentPassengers :"+train.currentPassengers);
+        
+                System.out.println("-----------------------------------");
+        
+                Train train1 = new Train();
+		train1.trainNumber = 1127;
+		train1.destination = "mysore";
+		train1.capacity = 3000;
+		train1.trainType = " express";
+		train1.currentPassengers = 1100;
+		System.out.println("trainNumber :"+train1.trainNumber);
+	        System.out.println("destination :"+train1.destination);
+	        System.out.println("capacity :"+train1.capacity);
+	        System.out.println("trainType :"+train1.trainType);
+	        System.out.println("currentPassengers :"+train1.currentPassengers);
+	
+                System.out.println("-----------------------------------");
+        
+                Train train2 = new Train();
+		train2.trainNumber = 14245;
+		train2.destination = "chennai";
+		train2.capacity = 4000;
+		train2.trainType = " ordinary";
+		train2.currentPassengers = 1500;
+		System.out.println("trainNumber :"+train2.trainNumber);
+	        System.out.println("destination :"+train2.destination);
+	        System.out.println("capacity :"+train2.capacity);
+	        System.out.println("trainType :"+train2.trainType);
+	        System.out.println("currentPassengers :"+train2.currentPassengers);
+		
+                System.out.println("-----------------------------------");
+        
+                Train train3 = new Train();
+		train3.trainNumber = 2574;
+		train3.destination = "new delhi";
+		train3.capacity = 4400;
+		train3.trainType = " ordinary";
+		train3.currentPassengers = 1700;
+		System.out.println("trainNumber :"+train3.trainNumber);
+	        System.out.println("destination :"+train3.destination);
+	        System.out.println("capacity :"+train3.capacity);
+	        System.out.println("trainType :"+train3.trainType);
+	        System.out.println("currentPassengers :"+train3.currentPassengers);
+                System.out.println("-----------------------------------");
+        
+                Train train4 = new Train();
+		train4.trainNumber = 25632;
+		train4.destination = "ballari";
+		train4.capacity = 2500;
+		train4.trainType = " ordinary";
+		train4.currentPassengers = 1200;
+		System.out.println("trainNumber :"+train4.trainNumber);
+	        System.out.println("destination :"+train4.destination);
+	        System.out.println("capacity :"+train4.capacity);
+	        System.out.println("trainType :"+train4.trainType);
+	        System.out.println("currentPassengers :"+train4.currentPassengers);
+        
+                System.out.println("-----------------------------------");
+        
+                Train train5 = new Train();
+		train5.trainNumber = 6357;
+		train5.destination = "hubli";
+		train5.capacity = 2800;
+		train5.trainType = " express";
+		train5.currentPassengers = 1300;
+		System.out.println("trainNumber :"+train5.trainNumber);
+	        System.out.println("destination :"+train5.destination);
+	        System.out.println("capacity :"+train5.capacity);
+	        System.out.println("trainType :"+train5.trainType);
+	        System.out.println("currentPassengers :"+train5.currentPassengers);
+                System.out.println("-----------------------------------");
+        
+                Train train6 = new Train();
+		train6.trainNumber = 4427;
+		train6.destination = "hyderabad";
+		train6.capacity = 4800;
+		train6.trainType = " express";
+		train6.currentPassengers = 2500;
+		System.out.println("trainNumber :"+train6.trainNumber);
+	        System.out.println("destination :"+train6.destination);
+	        System.out.println("capacity :"+train6.capacity);
+	        System.out.println("trainType :"+train6.trainType);
+	        System.out.println("currentPassengers :"+train6.currentPassengers);
+                System.out.println("-----------------------------------");
+        
+                Train train7 = new Train();
+		train7.trainNumber = 5377;
+		train7.destination = "bombay";
+		train7.capacity = 4500;
+		train7.trainType = " express";
+		train7.currentPassengers = 3000;
+		System.out.println("trainNumber :"+train7.trainNumber);
+	        System.out.println("destination :"+train7.destination);
+	        System.out.println("capacity :"+train7.capacity);
+	        System.out.println("trainType :"+train7.trainType);
+	        System.out.println("currentPassengers :"+train7.currentPassengers);
+                System.out.println("-----------------------------------");
+        
+                Train train8 = new Train();
+		train8.trainNumber = 6357;
+		train8.destination = "mumbai";
+		train8.capacity = 5000;
+		train8.trainType = " ordinary";
+		train8.currentPassengers = 4000;
+		System.out.println("trainNumber :"+train8.trainNumber);
+	        System.out.println("destination :"+train8.destination);
+	        System.out.println("capacity :"+train8.capacity);
+	        System.out.println("trainType :"+train8.trainType);
+	        System.out.println("currentPassengers :"+train8.currentPassengers);
+                System.out.println("-----------------------------------");
+        
+                Train train9 = new Train();
+		train9.trainNumber = 7537;
+		train9.destination = "vijayawada";
+		train9.capacity = 4500;
+		train9.trainType = " express";
+		train9.currentPassengers = 3500;
+		System.out.println("trainNumber :"+train9.trainNumber);
+	        System.out.println("destination :"+train9.destination);
+	        System.out.println("capacity :"+train9.capacity);
+	        System.out.println("trainType :"+train9.trainType);
+	        System.out.println("currentPassengers :"+train9.currentPassengers);
+                
+	}
+
+}
